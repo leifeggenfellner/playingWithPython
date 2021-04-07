ed8ac2d60596e87a6605439cae66a0e8e5b38f8d
Leizzz69 <leif.eggenfellner@gmail.com>
2021-04-07 08:11:12 +0200

    removed the sorting from the file.

--- searchAlgorithms/binarySearchAlgorithm/main.py
+++ searchAlgorithms/binarySearchAlgorithm/main.py
@@ -1,34 +1,7 @@
 from math import floor
 
-def partition(arr, low, high):
-    pivot = arr[high]
-
-    i = (low - 1)
-
-    for j in range(low, high):
-        if arr[j] <= pivot:
-            i = i + 1
-            arr[i], arr[j] = arr[j], arr[i]
-
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-
-    return (i + 1)
-
-
-def quickSort(arr, low, high):
-    if len(arr) == 1:
-        return arr
-    
-    if low < high:
-        pi = partition(arr, low, high)
-
-        quickSort(arr, low, pi - 1)
-        quickSort(arr, pi + 1, high)
-
-# Finds index of sorted array. Sorts array if it isn't sorted.
+# Finds index of sorted array.
 def binarySearch(arr, n, target):
-    if not all(arr[i] <= arr[i+1] for i in range(len(arr)-1)):
-        quickSort(arr, 0, len(arr) - 1)
 
     l = 0
     r = n - 1
