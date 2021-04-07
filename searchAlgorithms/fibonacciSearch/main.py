af612074226bf0035cc332fa73916f45fbc2a034
Leizzz69 <leif.eggenfellner@gmail.com>
2021-04-07 11:37:26 +0200

    wrote a script for fibonacci search

---
+++ searchAlgorithms/fibonacciSearch/main.py
@@ -0,0 +1,31 @@
+def fibonacciSearch(arr, length, target):
+    f2 = 0
+    f1 = 1
+    fm = f2 + f1
+    
+    offset = -1
+
+    while fm < length:
+        f2 = f1
+        f1 = fm
+        fm = f2 + f1
+    
+    while fm > 1:
+        i = min(offset + f2, length - 1)
+
+        if arr[i] < target:
+            fm = f1
+            f1 = f2
+            f2 = fm - f1
+            offset = i
+        elif arr[i] > target:
+            fm = f2
+            f1 = f1 - f2
+            f2 = fm - f1
+        else:
+            return i
+
+    if f1 and arr[offset + 1] == target:
+        return offset + 1
+    
+    return "Element not in array"
