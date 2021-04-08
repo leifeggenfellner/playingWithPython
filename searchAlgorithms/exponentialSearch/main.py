f0431351675de2ed4223fc42922669aeb22716cb
Leizzz69 <leif.eggenfellner@gmail.com>
2021-04-08 14:23:34 +0200

    made script for exponential search algorithm

---
+++ searchAlgorithms/exponentialSearch/main.py
@@ -0,0 +1,27 @@
+from math import floor
+
+# Expects a sorted array, the length of the array, and the number you wish to find the index of.
+def binarySearch(arr, length, target):
+
+    l = 0
+    r = length - 1
+
+    while l <= r:
+        m = floor((l + r) / 2)
+        if arr[m] < target:
+            l = m + 1
+        elif arr[m] > target:
+            r = m - 1
+        else:
+            return m
+    return -1
+
+def exponentialSearch(arr, length, target):
+    if length == 0:
+        return -1
+    
+    bound = 1
+    while bound < length and arr[bound] < target:
+        bound *= 2
+    
+    return binarySearch(arr, min(bound + 1, length), target)
