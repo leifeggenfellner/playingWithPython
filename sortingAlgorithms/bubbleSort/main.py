1ee8b01610315617e4d1548950efbedf80d502d0
Leizzz69 <leif.eggenfellner@gmail.com>
2021-04-12 12:41:39 +0200

    wrote bubbleSort algorithm

---
+++ sortingAlgorithms/bubbleSort/main.py
@@ -0,0 +1,10 @@
+def bubbleSort(arr):
+    length = len(arr)
+
+    while length > 1:
+        newn = 0
+        for i in range(1, length):
+            if arr[i - 1] > arr[i]:
+                arr[i - 1], arr[i] = arr[i], arr[i - 1]
+                newn = i
+        length = newn
